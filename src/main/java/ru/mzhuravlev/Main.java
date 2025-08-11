717145e84d8e0f60a7c1ee76eb47a86b604419da
MikhailZhur <miha99_66@mail.ru>
2025-08-11 16:05:14 +0500

    sdfsdgsdgdgs

--- src/main/java/ru/mzhuravlev/Main.java
+++ src/main/java/ru/mzhuravlev/Main.java
@@ -3,8 +3,11 @@ package ru.mzhuravlev;
 public class Main {
 
     private static String msg = "Hello world and my friend!";
-    
+
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(msg)
+        sfsdsdvsdv
+        sdvsv
+                sdvsdv;
     }
 }
