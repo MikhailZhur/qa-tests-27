5aef179a0a940f0f50fa0b67db40c3da43b99e92
mzhuravlev <mzhuravlev@diasoft.ru>
2025-08-13 07:49:35 +0500

    added final for msg

--- src/main/java/ru/mzhuravlev/Main.java
+++ src/main/java/ru/mzhuravlev/Main.java
@@ -2,9 +2,9 @@ package ru.mzhuravlev;
 
 public class Main {
 
-    private static String msg = "Hello world and my friend!";
+    private final static String msg = "Hello world and my friend!";
 
     public static void main(String[] args) {
-        System.out.println(msg)
+        System.out.println(msg);
     }
 }
